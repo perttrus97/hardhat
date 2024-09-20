c29838332c42b4442edcc59b25943d935bb5c769
Patricio Palladino <email@patriciopalladino.com>
2024-09-20 18:05:22 +0000

    Initial implementation of the Solidity config

--- v-next/hardhat/src/internal/builtin-plugins/solidity/type-extensions.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity/type-extensions.ts
@@ -1,11 +1,69 @@
 import "../../../types/config.js";
 
-declare module "@ignored/hardhat-vnext/types/config" {
+declare module "../../../types/config.js" {
+  export type SolidityUserConfig =
+    | string
+    | string[]
+    | SingleVersionSolidityUserConfig
+    | MultiVersionSolidityUserConfig
+    | BuildProfilesSolidityUserConfig;
+
+  export interface SolcUserConfig {
+    version: string;
+    settings?: any;
+  }
+
+  export interface MultiVersionSolcUserConfig {
+    compilers: SolcUserConfig[];
+    overrides?: Record<string, SolcUserConfig>;
+  }
+
+  export interface SingleVersionSolidityUserConfig extends SolcUserConfig {
+    dependenciesToCompile?: string[];
+    remappings?: string[];
+  }
+
+  export interface MultiVersionSolidityUserConfig
+    extends MultiVersionSolcUserConfig {
+    dependenciesToCompile?: string[];
+    remappings?: string[];
+  }
+
+  export interface BuildProfilesSolidityUserConfig {
+    profiles: Record<string, SolcUserConfig | MultiVersionSolcUserConfig>;
+    dependenciesToCompile?: string[];
+    remappings?: string[];
+  }
+
+  export interface HardhatUserConfig {
+    solidity?: SolidityUserConfig;
+  }
+
+  export interface SolcConfig {
+    version: string;
+    settings: any;
+  }
+
+  export interface SolidityBuildProfileConfig {
+    compilers: SolcConfig[];
+    overrides: Record<string, SolcConfig>;
+  }
+
+  export interface SolidityConfig {
+    profiles: Record<string, SolidityBuildProfileConfig>;
+    dependenciesToCompile?: string[];
+    remappings: string[];
+  }
+
+  export interface HardhatConfig {
+    solidity: SolidityConfig;
+  }
+
   export interface SourcePathsUserConfig {
     solidity?: string | string[];
   }
 
   export interface SourcePathsConfig {
-    solidity: string | string[];
+    solidity: string[];
   }
 }
